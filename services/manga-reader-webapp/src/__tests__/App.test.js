f00dbf903daa318b7bb02e0872692064c4b0c2c2
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-02-10 18:19:05 +0100

    Install React Icons + Begin mangas carousel

--- services/manga-reader-webapp/src/__tests__/App.test.js
+++ services/manga-reader-webapp/src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import renderer from "react-test-renderer";
 import App from "../App";
 
-it("Renders Hello World! on the App", () => {
+it("Renders Home on the App", () => {
   const component = renderer.create(<App />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
