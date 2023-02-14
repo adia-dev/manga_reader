c587419208d60f78d21ed7151b02aad9b24829c6
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-02-14 21:28:41 +0100

    remove the unused webapp

--- services/manga-reader-webapp/__mocks__/axios.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  get: jest.fn(() => Promise.resolve({ data: {} })),
-  post: jest.fn(() => Promise.resolve({ data: {} })),
-  put: jest.fn(() => Promise.resolve({ data: {} })),
-  delete: jest.fn(() => Promise.resolve({ data: {} })),
-};
