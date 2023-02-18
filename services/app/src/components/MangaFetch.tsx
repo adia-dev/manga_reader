22cf519537962d85eb9f9da2bec1b94098d913ca
pascal <paschy2002@gmail.com>
2023-02-18 21:36:57 +0100

    MangaFetch component fetches titles of the latest updated mangas

--- services/app/src/components/MangaFetch.tsx
+++ services/app/src/components/MangaFetch.tsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 
 
 type Props = {}
 
-const MangaFetch = (props: Props) => {
-    
 
+const MangaFetch: React.FC = (props: Props) => {
+
+    const [results, setResults] = useState<number[] | null>(null);
+
+    const handleSubmit = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.mangadex.org/manga?order[createdAt]=desc&limit=3`,
+          {}
+        );
+  
+        setResults(response.data.data.map((manga: any) => manga.attributes.title.en));
+
+      } catch (error) {
+        console.error(error);
+        alert("Error with the request.");
+      }
+    };
+
+    useEffect(() => {
+      handleSubmit();
+      
+    }, []);
     return (
-        <div className="text-neutral-50">MangaFetch</div>
-    )
-}
+      <>
+        
+        {results && (
+          <ul>
+            {results.map((title) => (
 
-export default MangaFetch
+              <li className='text-white' key={title}><img src="https://uploads.mangadex.org/covers/504cb09b-6f5d-4a2c-a363-6de16f8d96cc/51ebaf79-7c48-4b70-8303-a4d7a40e7887.jpg" alt='manga' className='h-52' />{title}</li>
+            ))}
+          </ul>
+        )}
+        <button value="Click me" />;
+
+      </>
+    );
+  };
+  
+
+export default MangaFetch;
+
+
+
