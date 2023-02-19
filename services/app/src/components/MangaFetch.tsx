f9eff6ce79870d343ce1bb1c8159a781c1886639
pascal <paschy2002@gmail.com>
2023-02-19 02:52:11 +0100

    Added Manga interface and simplified functions to send GET requests

--- services/app/src/components/MangaFetch.tsx
+++ services/app/src/components/MangaFetch.tsx
@@ -1,45 +1,97 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-
+import axios, { AxiosResponse } from 'axios';
 
 type Props = {}
+type Manga ={
+  id: string;
+  type: string;
 
+  attributes: {
+    title: {
+      en: string;
+    },
+    altTitles: {
+      [key: string]: string;
+    }[],
+    description: {
+      [key: string]: string;
+    },
+    isLocked: boolean,
+    links: {
+      [key: string]: string;
+    },
+    originalLanguage: string,
+    lastVolume: string,
+    lastChapter: string,
+    publicationDemographic: string,
+    status: string,
+    year: number,
+    contentRating: string,
+    tags: {
+      id: string,
+      type: string,
+      attributes: {
+        name: {
+          en: string;
+        },
+        description: {
+          [key: string]: string;
+        },
+        group: string,
+        version: number,
+      },
+      relationships: any[],
+    }[],
+    state: string,
+    chapterNumbersResetOnNewVolume: boolean,
+    createdAt: string,
+    updatedAt: string,
+    version: number,
+    availableTranslatedLanguages: any[],
+    latestUploadedChapter: any,
+  },
+  relationships: {
+    id: string,
+    type: string,
+  }[],
+}
+
+const getMangaList = async (url: string): Promise<Manga[]> => {
+  const response = await axios.get(url);
+  return response.data.data;
+};
 
 const MangaFetch: React.FC = (props: Props) => {
 
-    const [results, setResults] = useState<number[] | null>(null);
+ 
+    const [mangaList, setMangaList] = useState<Manga[]>([]);
 
-    const handleSubmit = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.mangadex.org/manga?order[createdAt]=desc&limit=3`,
-          {}
-        );
   
-        setResults(response.data.data.map((manga: any) => manga.attributes.title.en));
+  
+    useEffect(() => {
+      const fetchMangaList = async () => {
+        const url = "https://api.mangadex.org/manga?order[createdAt]=desc&limit=5";
+        const mangaList = await getMangaList(url);
+        setMangaList(mangaList);
+      };
+      fetchMangaList();
+    }, []);
 
-      } catch (error) {
-        console.error(error);
-        alert("Error with the request.");
-      }
-    };
 
     useEffect(() => {
-      handleSubmit();
-      
-    }, []);
+      console.log(mangaList);
+    }, [mangaList]);
     return (
       <>
         
-        {results && (
+        
           <ul>
-            {results.map((title) => (
+            {mangaList.map((manga) => (
 
-              <li className='text-white' key={title}><img src="https://uploads.mangadex.org/covers/504cb09b-6f5d-4a2c-a363-6de16f8d96cc/51ebaf79-7c48-4b70-8303-a4d7a40e7887.jpg" alt='manga' className='h-52' />{title}</li>
+              <li className='text-white' key={manga.id}><img src="https://uploads.mangadex.org/covers/504cb09b-6f5d-4a2c-a363-6de16f8d96cc/51ebaf79-7c48-4b70-8303-a4d7a40e7887.jpg" alt='manga' className='h-52' />{manga.attributes.title.en}</li>
             ))}
           </ul>
-        )}
+        
         <button value="Click me" />;
 
       </>
