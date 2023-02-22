38d99d08eb06c639a7335583563c3b3072212ca6
pascal <paschy2002@gmail.com>
2023-02-22 01:04:32 +0100

    Added links to MangaPage for each manga rendered in Row component and removed unused imports in Row

--- services/app/src/components/Row.tsx
+++ services/app/src/components/Row.tsx
@@ -2,7 +2,7 @@ import React ,{ useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import axios, { AxiosResponse } from 'axios';
 import Manga from "../interfaces/Manga";
-import mangas from "../data/mangasResponse.json";
+
 
 type Props = {
     name: string,
@@ -40,11 +40,12 @@ const Row = (props: Props) => {
                             <div className='relative h-[250px] aspect-[0.7] brightness-75 bg-[#222] shadow-xl rounded-sm m-2 cursor-pointer hover:scale-105 group transition-all duration-300 hover:z-50 hover:border-[#fff] hover:border-2 hover:shadow-2xl hover:brightness-100 m-4'
                                 key={manga.id}
                             >   
+                                <Link to={`manga/${manga.id}`}>
                                     <img src={`https://uploads.mangadex.org/covers/${manga.id}/${manga.relationships.find((rel)=>rel.type==="cover_art")?.attributes?.fileName}`} alt='manga' className='w-full h-full object-cover' />
                                     <p className='absolute bottom-5 left-2 p-2 transition-all duration-300 delay-300 text-white group-hover:z-50'>{manga.attributes.title.en}</p>
                                 
                                 <div className='absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-[#222] to-transparent'></div>
-                                
+                                </Link>
                             </div>
                         ))
                     }
