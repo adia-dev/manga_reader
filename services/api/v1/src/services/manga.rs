d40023e8ad8edb8afba8b392671f51fc4f6abec1
Abdoulaye Dia <abdoudu78130@gmail.com>
2024-02-12 01:41:13 +0100

    Fixed the damn project

--- services/api/v1/src/services/manga.rs
+++ services/api/v1/src/services/manga.rs
@@ -1,6 +1,9 @@
-// manga related services inside of a /manga scope
-use actix_web::{get, web, HttpResponse};
+// src/manga/routes.rs
+
+use actix_web::{get, web, HttpResponse, Result};
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MangaQueryParams {
@@ -11,115 +14,64 @@ pub struct MangaQueryParams {
     order: Option<Vec<String>>,
 }
 
-#[get("/{title}")]
-pub async fn get_manga_by_title(
-    title: web::Path<String>,
-    _params: web::Query<MangaQueryParams>,
-) -> HttpResponse {
-    let url = format!("{}/manga?title={}&limit=3&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&includes[]=cover_art&order[relevance]=desc", get_mangadex_base_url(), title);
-
-    let response = match reqwest::get(&url).await {
-        Ok(response) => response.text().await.unwrap(),
-        Err(e) => {
-            println!("Error: {}", e);
-            return HttpResponse::NotFound().body("Manga not found");
-        }
-    };
-
-    let manga: serde_json::Value = serde_json::from_str(&response).unwrap();
-    HttpResponse::Ok().json(manga)
-}
-
-#[get("/order/{order}")]
-pub async fn get_manga_by_order(order: web::Path<String>) -> HttpResponse {
-    println!("order: {}", order);
-    // /manga?order[followedCount]=desc&limit=20&includes[]=cover_art&contentRating[]=safe&hasAvailableChapters=true
-    let url = format!("{}/manga?order[{}]=desc&limit=20&includes[]=cover_art&contentRating[]=safe&hasAvailableChapters=true", get_mangadex_base_url(), order);
-
-    let response = match reqwest::get(&url).await {
-        Ok(response) => response.text().await.unwrap(),
-        Err(e) => {
-            println!("Error: {}", e);
-            return HttpResponse::NotFound().body("Manga not found");
-        }
-    };
-
-    let manga: serde_json::Value = serde_json::from_str(&response).unwrap();
-
-    HttpResponse::Ok().json(manga)
-}
-
-#[get("/id/{id}")]
-pub async fn get_manga_by_id(id: web::Path<String>) -> HttpResponse {
-    let url = format!("{}/manga/{}", get_mangadex_base_url(), id);
-
-    let response = match reqwest::get(&url).await {
-        Ok(response) => response.text().await.unwrap(),
-        Err(e) => {
-            println!("Error: {}", e);
-            return HttpResponse::NotFound().body("Manga not found");
-        }
-    };
-    let manga: serde_json::Value = serde_json::from_str(&response).unwrap();
-    HttpResponse::Ok().json(manga)
-}
-
-#[get("/id/{id}/stats")]
-pub async fn get_manga_stats_by_id(id: web::Path<String>) -> HttpResponse {
-    println!("id: {}", id);
-    let url = format!("{}/statistics/manga/{}", get_mangadex_base_url(), id);
-
-    let response = match reqwest::get(&url).await {
-        Ok(response) => response.text().await.unwrap(),
-        Err(e) => {
-            println!("Error: {}", e);
-            return HttpResponse::NotFound().body("Manga not found");
-        }
-    };
-    let manga: serde_json::Value = serde_json::from_str(&response).unwrap();
-    HttpResponse::Ok().json(manga)
-}
-
-// combine get manga by title to get the id and then get the manga stats by id
-#[get("{title}/stats")]
-pub async fn get_manga_stats_by_title(title: web::Path<String>) -> HttpResponse {
-    let url = format!("{}/manga?title={}&limit=3&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&includes[]=cover_art&order[relevance]=desc", get_mangadex_base_url(), title);
-
-    let response = match reqwest::get(&url).await {
-        Ok(response) => response.text().await.unwrap(),
-        Err(e) => {
-            println!("Error: {}", e);
-            return HttpResponse::NotFound().body("Manga not found");
-        }
-    };
-
-    let mut manga: serde_json::Value = serde_json::from_str(&response).unwrap();
-    // map the manga response and add the stats to the data object
-    for manga in manga["data"].as_array_mut().unwrap() {
-        let id = manga["id"].as_str().unwrap();
-        let stats_url = format!("{}/statistics/manga/{}", get_mangadex_base_url(), id);
-        let stats_response = match reqwest::get(&stats_url).await {
-            Ok(response) => response.text().await.unwrap(),
-            Err(e) => {
-                println!("Error: {}", e);
-                return HttpResponse::NotFound().body("Manga not found");
-            }
-        };
-        let stats: serde_json::Value = serde_json::from_str(&stats_response).unwrap();
-        manga["statistics"] = stats["statistics"].clone();
-    }
-
-    HttpResponse::Ok().json(manga)
+async fn fetch_manga_data(url: String) -> Result<Value, actix_web::Error> {
+    let client = Client::new();
+    client
+        .get(url)
+        .header("User-Agent", "rust-manga-reader-project")
+        .send()
+        .await
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?
+        .json::<Value>()
+        .await
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))
 }
 
 fn get_mangadex_base_url() -> String {
-    dotenv::var("MANGADEX_API_BASE_URL").unwrap()
+    dotenv::var("MANGADEX_API_BASE_URL").expect("MANGADEX_API_BASE_URL must be set")
 }
 
-fn format_query_params_includes(includes: Vec<String>) -> String {
-    let mut formatted = String::new();
-    for include in includes {
-        formatted.push_str(&format!("&includes[]={}", include));
+#[get("/{title}")]
+pub async fn get_manga_by_title(title: web::Path<String>) -> Result<HttpResponse> {
+    let url = format!("{}/manga?title={}&limit=3&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&includes[]=cover_art&order[relevance]=desc", get_mangadex_base_url(), title);
+    let manga = fetch_manga_data(url).await?;
+    Ok(HttpResponse::Ok().json(manga))
+}
+
+#[get("/order/{order}")]
+pub async fn get_manga_by_order(order: web::Path<String>) -> Result<HttpResponse> {
+    let url = format!("{}/manga?order[{}]=desc&limit=20&includes[]=cover_art&contentRating[]=safe&hasAvailableChapters=true", get_mangadex_base_url(), order);
+    let manga = fetch_manga_data(url).await?;
+    Ok(HttpResponse::Ok().json(manga))
+}
+
+#[get("/id/{id}")]
+pub async fn get_manga_by_id(id: web::Path<String>) -> Result<HttpResponse> {
+    let url = format!("{}/manga/{}", get_mangadex_base_url(), id);
+    let manga = fetch_manga_data(url).await?;
+    Ok(HttpResponse::Ok().json(manga))
+}
+
+#[get("/id/{id}/stats")]
+pub async fn get_manga_stats_by_id(id: web::Path<String>) -> Result<HttpResponse> {
+    let url = format!("{}/statistics/manga/{}", get_mangadex_base_url(), id);
+    let manga = fetch_manga_data(url).await?;
+    Ok(HttpResponse::Ok().json(manga))
+}
+
+#[get("/{title}/stats")]
+pub async fn get_manga_stats_by_title(title: web::Path<String>) -> Result<HttpResponse> {
+    let manga_data_url = format!("{}/manga?title={}&limit=3&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&includes[]=cover_art&order[relevance]=desc", get_mangadex_base_url(), title);
+    let mut manga = fetch_manga_data(manga_data_url).await?;
+    if let Some(data) = manga["data"].as_array_mut() {
+        for manga_entry in data {
+            if let Some(id) = manga_entry["id"].as_str() {
+                let stats_url = format!("{}/statistics/manga/{}", get_mangadex_base_url(), id);
+                if let Ok(stats) = fetch_manga_data(stats_url).await {
+                    manga_entry["statistics"] = stats["statistics"].clone();
+                }
+            }
+        }
     }
-    formatted
+    Ok(HttpResponse::Ok().json(manga))
 }
